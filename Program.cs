9637d7c37a2cc5b46089922677ead7e70b96d869
Annette Hawks <hawksannette@gmail.com>
2025-03-12 12:30:56 -0600

    First Column

--- Program.cs
+++ Program.cs
@@ -1,2 +1,89 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.SetCursorPosition(1, 1); //left top
+Console.WriteLine("__");
+
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.SetCursorPosition(1, 2); //Column 1
+Console.WriteLine("__");
+
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.SetCursorPosition(1, 3); //Column 1
+
+Console.WriteLine("__");
+
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.SetCursorPosition(1, 4); //Column 1
+Console.WriteLine("__");
+
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.SetCursorPosition(1, 5); //Column 1
+Console.WriteLine("__");
+
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.SetCursorPosition(1, 6); //Column 1
+Console.WriteLine("__");
+
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.SetCursorPosition(1, 7); //Column 1
+Console.WriteLine("__");
+
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.SetCursorPosition(1, 8); //Column 1
+Console.WriteLine("__");
+
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.SetCursorPosition(1, 9); //Column 1
+Console.WriteLine("__");
+
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.SetCursorPosition(1, 10); //Column 1
+Console.WriteLine("__");
+
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.SetCursorPosition(1, 11); //Column 1
+Console.WriteLine("__");
+
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.SetCursorPosition(1, 12); //Column 1
+Console.WriteLine("__");
+
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.SetCursorPosition(1, 13); //Column 1
+Console.WriteLine("__");
+
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.SetCursorPosition(1, 14); //Column 1
+Console.WriteLine("__");
+
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.SetCursorPosition(1, 15); //Column 1
+Console.WriteLine("__");
+
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.SetCursorPosition(1, 16); //Column 1
+Console.WriteLine("__");
+
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.SetCursorPosition(1, 17); //Column 1
+Console.WriteLine("__");
+
+
+
